20f2844fd4ae4dbfa3c50ffeffe7a14f4a33108d
liyouhong <rekrad@163.com>
2019-08-23 16:48:52 +0800

    example添加TestRunner测试代码和节点

--- examples/load_from_rawproject/main.go
+++ examples/load_from_rawproject/main.go
@@ -12,6 +12,27 @@ import (
 	. "github.com/rekrad/behavior3go/loader"
 )
 
+// 自定义action节点
+type TestRunner struct {
+	Action
+	info string
+	info2 int
+	info3 string
+	info4 int
+}
+
+func (this *TestRunner) Initialize(setting *BTNodeCfg) {
+	this.Action.Initialize(setting)
+	this.info = setting.GetPropertyAsString("prob1")
+	this.info2 = setting.GetPropertyAsInt("prob2")
+}
+
+func (this *TestRunner) OnTick(tick *Tick) b3.Status {
+	fmt.Printf("TestRunner.OnTick: prob1: %v prob2 %v p1: %v p2: %v\n", this.info, this.info2, this.info3, this.info4)
+	return b3.SUCCESS
+}
+
+
 func main() {
 	projectConfig, ok := LoadRawProjectCfg("example.b3")
 	if !ok {
@@ -22,6 +43,7 @@ func main() {
 	//自定义节点注册
 	maps := b3.NewRegisterStructMaps()
 	maps.Register("Log", new(LogTest))
+	maps.Register("TestRunner", new(TestRunner))
 
 	var firstTree *BehaviorTree
 	//载入
